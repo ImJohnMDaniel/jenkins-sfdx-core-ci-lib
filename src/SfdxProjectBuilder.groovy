a2baaad9044c30858104d475435239c700e8457d
John M. Daniel <imjohnmdaniel@ce-v.com>
2020-08-04 10:03:13 -0400

    does this work?

--- src/SfdxProjectBuilder.groovy
+++ src/SfdxProjectBuilder.groovy
@@ -85,7 +85,7 @@ class SfdxProjectBuilder implements Serializable {
 // TODO: Setup Kubernetes POD here
           // this.node(POD_LABEL) {
           // }
-          this.container('salesforcedx') {
+          this.container() {
             processStages()
           }
         }
