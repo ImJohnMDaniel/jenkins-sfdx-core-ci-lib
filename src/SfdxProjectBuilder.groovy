d3ebaec4898f9080c6ce02a59edd3ecf83e0722b
John M. Daniel <imjohnmdaniel@ce-v.com>
2021-01-05 16:55:26 -0500

    tweak

--- src/SfdxProjectBuilder.groovy
+++ src/SfdxProjectBuilder.groovy
@@ -921,19 +921,18 @@ class SfdxProjectBuilder implements Serializable {
     _.echo("Push To Scratch Org And Compile")
     try {
       _.echo( _.env.SFDX_JSON_TO_STDOUT )
-      def rmsg = _.sh returnStatus: true, script: "sfdx force:source:push --forceoverwrite --json --targetusername ${this.sfdxScratchOrgAlias} &> force-source-push.json"
-      // printf rmsg
+      def rmsg = _.sh returnStdout: true, script: "sfdx force:source:push --forceoverwrite --json --targetusername ${this.sfdxScratchOrgAlias}"
 
-      // def response = jsonParse( rmsg )
-      // debug( response )
+      def response = jsonParse( rmsg )
+      debug( response )
 
-      // if (response.status != 0) {
-      //     _.error "push failed -- ${response.message}"
-      // }
-      debug( rmsg )
-      if ( rmsg > 0 ) {
-        _.error('force:source:push failed')
+      if (response.status != 0) {
+          _.error "push failed -- ${response.message}"
       }
+      // debug( rmsg )
+      // if ( rmsg > 0 ) {
+      //   _.error('force:source:push failed')
+      // }
 
     }
     catch(ex) {
